ce8a62cb396464bfe75bbcf0a880fe35e6524334
Sammit Badodekar <badodekarsammit@gmail.com>
2025-04-13 20:37:56 +0530

    feat: create page init

--- apps/web/app/create/page.tsx
+++ apps/web/app/create/page.tsx
@@ -1,7 +1,14 @@
+import TiptapEditor from "@/components/editor/tiptap-editor";
 import React from "react";
 
 const Page = () => {
-  return <div>Create forms</div>;
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="max-w-[600px] w-full">
+        <TiptapEditor />
+      </div>
+    </div>
+  );
 };
 
 export default Page;

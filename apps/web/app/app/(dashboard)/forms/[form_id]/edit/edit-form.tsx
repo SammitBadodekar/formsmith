9542bb6d699fce2c901aae9febd348c80c58a113
Sammit Badodekar <badodekarsammit@gmail.com>
2025-07-26 22:45:57 +0530

    chore: make form edit page force dynamic

--- apps/web/app/app/(dashboard)/forms/[form_id]/edit/edit-form.tsx
+++ apps/web/app/app/(dashboard)/forms/[form_id]/edit/edit-form.tsx
@@ -18,6 +18,8 @@ import FormCustomization from "@/components/editor/components/form-customization
 import { formCustomizationAtom } from "@/app/app/(dashboard)/forms/[form_id]/atoms";
 import { useAtom } from "jotai";
 
+export const dynamic = "force-dynamic";
+
 const saveFormWithDebounce = debounce(async (callback, payload) => {
   if (callback && payload) {
     await callback(payload);

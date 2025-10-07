5492c838dd34b911ed3e56c8864199f61d865619
Sammit Badodekar <badodekarsammit@gmail.com>
2025-10-07 09:44:09 +0530

    feat: vercel speed insight

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
-import { cookies } from "next/headers";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({
@@ -28,6 +28,7 @@ export default function RootLayout({
       >
         {children}
         <Toaster position="top-center" />
+        <SpeedInsights />
       </body>
     </html>
   );
